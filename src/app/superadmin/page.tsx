309d612cce322f86065f2e029a104b2647a11018
mrdeee <merchantdaniel8@gmail.com>
2026-05-10 17:12:34 +0000

    user

--- src/app/superadmin/page.tsx
+++ src/app/superadmin/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import styles from "./UserForm.module.css";
+import styles from "./superadmin.module.css";
 
 interface WasteManagementInfo {
   CompanyName?: string;
