1f73af4f70abe2560d4e88a2add7b3823bd66c0d
mrdeee <merchantdaniel8@gmail.com>
2026-06-11 13:48:27 +0000

    civilsociety

--- src/app/civil-society/page.tsx
+++ src/app/civil-society/page.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import { auth, db } from '@/lib/firebase';
 import { ref, onValue, get, push, set } from 'firebase/database';
 import { useRouter } from 'next/navigation';
-import styles from './civilsociety.module.css';
+import styles from '..civil-society/civilsociet.module.css';
 
 interface WasteRequest {
   id: string;
